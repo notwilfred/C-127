29e9cb0122ed1c67c8f3a8707f8459a2e1df566e
notwilfred <129186730+notwilfred@users.noreply.github.com>
2023-10-21 18:02:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+peter_pan_song = "";
+HArry_potter_song = "";
+
+function preload(){
+    peter_pan_song=loadSound("music2.mp3");
+    HArry_potter_song=loadSound("music.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
